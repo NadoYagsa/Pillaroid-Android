c34938a53da37930ac3b7245e70b247b1965b2f5
youngniw <1999julie@naver.com>
2022-04-08 18:55:03 +0900

    [refactor] 일반적 액션바 통일 (#4)

--- app/src/main/java/com/nadoyagsa/pillaroid/SearchCameraActivity.java
+++ app/src/main/java/com/nadoyagsa/pillaroid/SearchCameraActivity.java
@@ -2,6 +2,8 @@ package com.nadoyagsa.pillaroid;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +23,12 @@ public class SearchCameraActivity extends AppCompatActivity {
             Objects.requireNonNull(actionBar).setDisplayShowCustomEnabled(true);
             actionBar.setDisplayShowTitleEnabled(false);
             actionBar.setDisplayHomeAsUpEnabled(false);
-        View customView = View.inflate(this, R.layout.actionbar_search_camera, null);
+        View customView = View.inflate(this, R.layout.actionbar_general, null);
+        /* 액션바 아이콘과 주제 설정 */
+        ImageView ivIcon = customView.findViewById(R.id.iv_ab_general_icon);
+        ivIcon.setImageResource(R.drawable.icon_camera);
+        TextView tvTopic = customView.findViewById(R.id.tv_ab_general_topic);
+        tvTopic.setText("의약품 촬영으로 검색");
         ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
         actionBar.setCustomView(customView, params);
     }
