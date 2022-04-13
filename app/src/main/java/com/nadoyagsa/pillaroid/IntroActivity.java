37ff8609133e3297b58c1243ef5aed6823aa54a7
soeunkk <luckyber1@naver.com>
2022-04-14 02:14:04 +0900

    [feat] 가이드 음량 조절, 보이스 속도 조절 TTS (#10)
    음량 기본값 8로 변경

--- app/src/main/java/com/nadoyagsa/pillaroid/IntroActivity.java
+++ app/src/main/java/com/nadoyagsa/pillaroid/IntroActivity.java
@@ -1,7 +1,10 @@
 package com.nadoyagsa.pillaroid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -11,6 +14,13 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String guideValue = preferences.getString("guideVolume", "8");  //음량
+
+        //설정한 음량값으로 음량 조절 (android.intent.action.MAIN에서 항상 설정 요망)
+        AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, Integer.parseInt(guideValue), AudioManager.FLAG_SHOW_UI);
 
         AppCompatButton btNext = findViewById(R.id.bt_intro_next);
         btNext.setOnClickListener(v -> {
