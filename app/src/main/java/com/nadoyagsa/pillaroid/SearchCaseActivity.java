51da2b3c217e64c436cc10736dbc7903ee958912
soeunkk <luckyber1@naver.com>
2022-04-29 03:32:09 +0900

    [feat] SearchCase 카메라 연동 (#23)
    SurfaceView 대신 PreviewView로 변경
    CameraX 사용으로 인한 디펜던시 추가
    촬영을 위해 Manifest에 CAMERA 추가

--- app/src/main/java/com/nadoyagsa/pillaroid/SearchCaseActivity.java
+++ app/src/main/java/com/nadoyagsa/pillaroid/SearchCaseActivity.java
@@ -1,18 +1,40 @@
 package com.nadoyagsa.pillaroid;
 
 import static android.speech.tts.TextToSpeech.ERROR;
+import static android.speech.tts.TextToSpeech.QUEUE_FLUSH;
 import static android.speech.tts.TextToSpeech.SUCCESS;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 public class SearchCaseActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSIONS = 1001;
+    private final String[] REQUIRED_PERMISSIONS = new String[] {
+            "android.permission.CAMERA"
+    };
+
     private TextToSpeech tts;
+
+    private PreviewView pvCaseCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +50,55 @@ public class SearchCaseActivity extends AppCompatActivity {
                 }
                 tts.setSpeechRate(SharedPrefManager.read("voiceSpeed", (float) 1));
 
-                tts.speak("후면 카메라가 켜졌습니다. 손에 의약품을 잡고 카메라 뒤로 위치시켜주세요.", TextToSpeech.QUEUE_FLUSH, null, null);
+                tts.speak("후면 카메라가 켜졌습니다. 손에 의약품을 잡고 카메라 뒤로 위치시켜주세요.", QUEUE_FLUSH, null, null);
             } else if (status != ERROR) {
                 Log.e("TTS", "Initialization Failed");
             }
         });
 
+        pvCaseCamera = findViewById(R.id.pv_search_case);
+
+        if(allPermissionsGranted()){
+            startCamera();  //카메라 실행
+        } else{ //모든 권한이 허가되지 않았다면 요청
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
         /* TODO: 용기를 인식 후 검색 결과 확인 */
         //startActivity(new Intent(this, MedicineResultActivity.class));
+    }
+
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().build();
+
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        preview.setSurfaceProvider(pvCaseCamera.getSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis, imageCapture);
+
+        //TODO: 볼륨 버튼 리스너 (imageCapture.takePicture로 캡처)
     }
 
     @Override
@@ -50,6 +113,29 @@ public class SearchCaseActivity extends AppCompatActivity {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private boolean allPermissionsGranted(){
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();  //권한 모두 허가되면 startCamera 실행
+            } else {
+                Log.e("Camera", "Permissions not granted by the user");
+                tts.speak("카메라 권한이 승인되지 않아 기능을 사용할 수 없습니다.", QUEUE_FLUSH, null, null);
+                this.finish();
+            }
         }
     }
 }
