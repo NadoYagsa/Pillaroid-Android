07fa23a94d602dd3a9cb9575acf36885c923b628
youngniw <1999julie@naver.com>
2022-08-18 23:03:08 +0900

    [feat] 낱알 촬영 관련 서버 api 연동

--- app/src/main/java/com/nadoyagsa/pillaroid/SearchPillActivity.java
+++ app/src/main/java/com/nadoyagsa/pillaroid/SearchPillActivity.java
@@ -7,9 +7,17 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SearchPillActivity extends AppCompatActivity {
     private TextToSpeech tts;
@@ -31,6 +39,25 @@ public class SearchPillActivity extends AppCompatActivity {
                 tts.speak("후면 카메라가 켜졌습니다. 손에 알약을 놓고 카메라 뒤로 위치시켜주세요.", TextToSpeech.QUEUE_FLUSH, null, null);
             } else if (status != ERROR) {
                 Log.e("TTS", "Initialization Failed");
+            }
+        });
+
+        // TODO: 알약 이미지 첨부 방식
+        MultipartBody.Part pillImage = MultipartBody.Part.createFormData("pillImage", /*파일 이름.확장자*/, RequestBody.create(MediaType.parse("multipart/form-data"), /*파일 내용 혹은 파일 byte[]*/));
+        PillaroidAPIImplementation.getApiService().postPillByImage(pillImage).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if (response.code() == 200) {
+                    // TODO: 성공 시 내용
+                }
+                else {
+                    // TODO: 실패 시 내용
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+                // TODO: 실패 시 내용
             }
         });
 
